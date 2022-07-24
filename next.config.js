11fd4ba2d4bbb5f5528e0494dd92254b34ac5813
kumaworld <kumaworld123@gmail.com>
2022-07-24 05:49:42 -0300

    Export path map dynamic

--- next.config.js
+++ next.config.js
@@ -1,3 +1,15 @@
+const fs = require('fs');
+const { join } = require('path')
+
+const postsDirectory = join(process.cwd(), 'assets/nft')
+
+function getAllNfts([] = []) {
+  const nfts = fs.readdirSync(postsDirectory)
+  return nfts
+    .map((nft, index) => getNftById(index.toString()))
+}
+
+
 module.exports = {
   reactStrictMode: true,
 
@@ -5,5 +17,16 @@ module.exports = {
     config.resolve.fallback = { fs: false };
 
     return config;
+  },
+  exportPathMap: async function () {
+      let paths = {}
+
+      const nfts = getAllNfts()
+
+      nfts.forEach((nft, index) => {
+        paths[`/puzzles/${index}`] = { page: '/puzzles', query: { id: index } }
+      })
+
+      return paths
   },
 }
