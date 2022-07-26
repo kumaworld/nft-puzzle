763ef0ade36ef5884a89aefb2a0826f5a684c8f3
kumaworld <kumaworld123@gmail.com>
2022-07-25 22:49:22 -0300

    Found the problem

--- pages/api/scores.ts
+++ pages/api/scores.ts
@@ -1,17 +1,19 @@
-import cors from 'cors';
 import { NextApiRequest, NextApiResponse } from 'next';
 import {connectToDatabase} from '../../config/mongodb';
-import { useMiddleware } from '../../lib/middleware';
+import { cors, useMiddleware } from '../../lib/middleware';
 import { NUMBER_OF_PLAYERS_RANKING } from '../../utils/constants';
 // eslint-disable-next-line import/no-anonymous-default-export
 export default async (req: NextApiRequest, res: NextApiResponse) => {
+  console.log('passa')
   await useMiddleware(req, res, cors);
 
   const { id } = req.query;
+  console.log('e')
 
   if (!id) return res.json("Página não encontrada!")
 
   const { db } = await connectToDatabase();
+  console.log('o')
 
   const scores = await db
     .collection("scores")
@@ -19,6 +21,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     .sort({ time: -1 })
     .limit(NUMBER_OF_PLAYERS_RANKING)
     .toArray()
+    console.log('u')
 
   return res.status(200).json({ scores: scores })
 }
